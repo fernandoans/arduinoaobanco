c3227763be58ecefa2ea2e9c8dc847b3b6d07a97
Fernando Anselmo <fernando.anselmo74@gmail.com>
2022-03-02 09:38:35 -0300

    [Fix] Acertos no código

--- Fontes/leitura/leitura.ino
+++ Fontes/leitura/leitura.ino
@@ -1,5 +1,7 @@
+// C++ code
+//
 #define LDR A0
-#define LM35 A1
+#define TMP335 A1
 
 int lums = 0;
 float tensao;
@@ -8,16 +10,18 @@ float grauF;
 
 void setup() {
   pinMode(LDR, INPUT);
-  pinMode(LM35, INPUT);
-  Serial.begin(9600);
+  pinMode(TMP335, INPUT);
+  Serial.begin(9600); // bounds
 }
 
 void loop() {
   lums = map(analogRead(LDR), 244, 1100, 0, 100);
-  tensao = (float(analogRead(LM35))*5)/1023; // 5V e leitura analógica 0 a 1023
-  grauC = tensao / 0.010; // 0,010 mV
+  tensao = (float(analogRead(TMP335)*5)/1024); // 5V e Leitura Analógica 0 a 1023 -> 1024
+  grauC = tensao/ 0.010; // 0,010 mV
   grauF = grauC * (9.0/5.0) + 32.0;
-  Serial.println("{\"lums\":" + String(lums) + ", \"grauC\":" 
-    + String(grauC) + ", \"grauF\":" + String(grauF) + "}"); 
-  delay(5000); // 5 segundos
+  
+  // {"lums": 99, "grauC": 24.3, "grauF": 78.3} 
+  
+  Serial.println("{\"lums\":" + String(lums) + ", \"grauC\":" + String(grauC) + ", \"grauF\":" + String(grauF) + "}");
+  delay(2000);
 }
